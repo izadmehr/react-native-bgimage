6da1abaf89d23971c4a7ed866281f4d79dba4423
Meysam izadmehr <meysam.s@gmail.com>
2017-05-22 11:11:00 +0430

    add iOS support
    For iOS, image send by source property

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import { Image } from 'react-native';
+
+const BgImage = (props) => {
+  return <Image
+    resizeMode="repeat"
+    {...props}
+  >
+    {props.children}
+  </Image>;
+};
+
+module.exports = BgImage;
